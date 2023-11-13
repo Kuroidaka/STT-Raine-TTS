3af17d89532f27cf92b12531f20761bea303bec5
Canh <pcanhgm@gmail.com>
2023-11-13 23:46:56 +0700

    update trigger API from prompt

--- index.py
+++ index.py
@@ -4,6 +4,10 @@ from tempfile import TemporaryFile
 from playsound import playsound
 import os
 import tempfile
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize recognizer class
 r = sr.Recognizer()
@@ -11,7 +15,7 @@ r = sr.Recognizer()
 def record_text():
     while True:
     # Reads the audio file
-        with sr.Microphone(device_index = 3) as source:
+        with sr.Microphone() as source:
             print("Start speaking, I'm all ears!")
 
             r.adjust_for_ambient_noise(source, duration= 0.2)
@@ -32,4 +36,26 @@ def speak_text(text, lang='en', slow=False):
     
 while(1):
     text = record_text()
-    speak_text(text, 'en-uk')
+    origin_url = os.getenv("ORIGIN_URL", "http://localhost:8000")
+    url = f'{origin_url}/api/v1/chatgpt/ask'
+    
+    headers = {'Content-Type': 'application/json'}
+    payload = {
+        'data': {
+            'content': text,
+            'guildId': os.environ['GUILD_ID']
+        },
+        'maxTokenEachScript': 400,
+        'curUser': {
+            'globalName': os.environ['USER_GLOBALNAME'],
+            'id': os.environ['USER_ID'],
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        print(response.json()['data'])
+        speak_text(response.json()['data'], 'en-uk')
+    else:
+        print("Oops, no pizza. Let's try again!")
